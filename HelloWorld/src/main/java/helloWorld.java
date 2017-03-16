8d54a97aecf46a91d9d10142ac6d4eeebd3ae3a1
Zalisekile <zalisky@gmail.com>
2017-03-16 08:35:22 +0200

    Adding Hello World

---
+++ HelloWorld/src/main/java/helloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Zalisekile on 2017/03/15.
+ */
+public class helloWorld
+{
+    public static void main(String[]args)
+    {
+        System.out.println("Hello Beautiful World");
+    }
+}
